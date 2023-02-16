89a32d069a55c61e1e40498f72b78823b3b29393
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-16 17:23:14 +0530

    Check wheather a Number is prime or not

---
+++ PrimeNo.cs
@@ -0,0 +1,30 @@
+﻿namespace LogicProgrammingProblems
+{
+    public class PrimeNo
+    {
+        public static void CheckPrimeNumber()
+        {
+            int factors = 1;
+            Console.WriteLine("Enter a Number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for( int i = 2; i<=num; i++ )
+            {
+                if (num % i == 0)
+                {
+                    factors ++; 
+                }
+        
+            }
+            if ( factors == 2 ) {
+
+                Console.WriteLine($"{num} is a prime no");
+
+            }
+            else
+            {
+                Console.WriteLine("Not a prime No.");
+            }
+        }
+
+    }
+}
