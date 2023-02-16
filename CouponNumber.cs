5e808f6fa2a0f7dea42a70ff4f597e067a9cd1c5
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-16 19:02:34 +0530

    Coupon Number Program

---
+++ CouponNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicProgrammingProblems
+{
+    public  class CouponNumber
+
+    {
+
+        public static void DistinctCouponNumber()
+        {
+            Console.WriteLine("Enter total number of distinct Coupon Numbers you want: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] couponNumber = new int[size];
+            Random random = new Random();
+
+            for (int i = 0; i < size; i++)
+            {
+                couponNumber[i] = random.Next(1, 100);
+
+
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (couponNumber[j] == couponNumber[i])
+                    {
+                        i--;
+                    }
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Coupon numer {i + 1}: {couponNumber[i]}");
+            }
+
+
+
+        }
+    }
+}
